0b7d278bd1b82dc5cc06a7b645a52af9ed1bbb93
Melriken <GFSnilrem@yahoo.com>
2021-05-13 15:09:40 -0700

    Update loader.lua
    Update to token spawning function to always spawn token to the right of the card (regardless of rotation of the card) and add hook for other project(s)

--- loader.lua
+++ loader.lua
@@ -253,14 +253,11 @@ local function jsonForCardFace(face, position)
 
             function getToken(i)
                 token = tokens[i]
-                position = self.getPosition()
-                position.y = position.y + 0.1
-                position.x = position.x + 2.5
                 spawnObject({
                     type = "Card",
                     sound = false,
                     rotation = self.getRotation(),
-                    position = position,
+                    position = self.positionToWorld({-2.2,0.1,0}),
                     scale = self.getScale(),
                     callback_function = (function(obj)
                         obj.memo = ""
@@ -270,6 +267,9 @@ local function jsonForCardFace(face, position)
                             face = token.front,
                             back = token.back
                         })
+                        if (parent) then
+                          parent.call("CAddButtons",{obj, self})
+                        end
                     end)
                 })
             end
